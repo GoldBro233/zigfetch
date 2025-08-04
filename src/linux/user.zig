2d1c7506d89484b5923de22633aee917fa2c221e
utox39 <francescomoccaldi39@gmail.com>
2025-08-04 02:37:52 +0200

    feat(shell): parse bash shell version

--- src/linux/user.zig
+++ src/linux/user.zig
@@ -22,7 +22,25 @@ pub fn getShell(allocator: std.mem.Allocator) ![]u8 {
 
     _ = try child.wait();
 
+    if (std.mem.indexOf(u8, shell, "bash") != null) {
+        const bash_version = parseBashVersion(allocator, output);
+        defer allocator.free(output);
+        return try std.fmt.allocPrint(allocator, "{s} {s}", .{ "bash", bash_version.? });
+    }
+
     return output;
+}
+
+fn parseBashVersion(allocator: std.mem.Allocator, shell_version_output: []u8) ?[]u8 {
+    _ = allocator;
+    const end_index = std.mem.indexOf(u8, shell_version_output, "(");
+    if (end_index == null) return null;
+
+    const version_keyword = "version ";
+    const version_keyword_index = std.mem.indexOf(u8, shell_version_output[0..end_index.?], version_keyword);
+    if (version_keyword_index == null) return null;
+
+    return shell_version_output[version_keyword_index.? + version_keyword.len .. end_index.?];
 }
 
 pub fn getTerminalName(allocator: std.mem.Allocator) ![]u8 {
