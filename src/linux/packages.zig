b9dc58fabe741d526cafa376f263ac7bc7982930
utox39 <francescomoccaldi39@gmail.com>
2025-12-06 19:28:12 +0100

    feat(linux-packages): add Nix packages count

--- src/linux/packages.zig
+++ src/linux/packages.zig
@@ -13,3 +13,18 @@ fn countFlatpaks(allocator: std.mem.Allocator) !usize {
 
     return try std.fmt.parseInt(usize, result_trimmed, 10);
 }
+
+fn countNixPackages(allocator: std.mem.Allocator) !usize {
+    // nix-store --query --requisites /run/current-system | wc -l
+    const result = try std.process.Child.run(.{ .allocator = allocator, .argv = &[_][]const u8{
+        "sh",
+        "-c",
+        "nix-store --query --requisites /run/current-system | wc -l",
+    } });
+
+    const result_stdout = result.stdout;
+    const result_trimmed = std.mem.trim(u8, result_stdout, "\n");
+    defer allocator.free(result_stdout);
+
+    return try std.fmt.parseInt(usize, result_trimmed, 10);
+}
