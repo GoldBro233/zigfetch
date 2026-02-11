51f6ddc2d1b0fe282c136109dd209551de3dfc5d
utox39 <francescomoccaldi39@gmail.com>
2026-02-11 18:33:03 +0100

    refactor(linux-user): use new Environ method

--- src/linux/user.zig
+++ src/linux/user.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
-pub fn getUsername(allocator: std.mem.Allocator) ![]u8 {
-    const username = try std.process.getEnvVarOwned(allocator, "USER");
-    return username;
+pub fn getUsername(allocator: std.mem.Allocator, environ: std.process.Environ) ![]u8 {
+    return try std.process.Environ.getAlloc(environ, allocator, "USER");
 }
 
 pub fn getShell(allocator: std.mem.Allocator) ![]u8 {
